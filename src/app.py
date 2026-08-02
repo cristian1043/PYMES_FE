ecf3888b1296f534d07c5d05ab09b941a701e22f
DUBONACO <garavitojimenezcristian@gmail.com>
2026-08-01 22:30:44 -0500

    yes

--- src/app.py
+++ src/app.py
@@ -3,6 +3,6 @@ from src import create_app
 app = create_app('development')
 
 if __name__ == '__main__':
-    print("frontend corriendo en http://localhost:5000")
-    app.run (debug=True, port=5000)
+    print("frontend corriendo en http://localhost:5001")
+    app.run (debug=True, port=5001)
     
--- src/config/config.py
+++ src/config/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'frontend-secret')
-    API_BASE_URL = os.getenv('API_BASE_URL', 'http://localhost:5000/api')
+    API_BASE_URL = os.getenv('API_BASE_URL', 'http://localhost:5001/api')
 
 class DevelopmentConfig(Config):
     DEBUG = True
